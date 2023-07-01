c4565a840ef1d1e22a8b01ab4d8d37046cabfd9d
Dmytro Chyr <dima543439@gmail.com>
2023-07-02 00:21:42 +0300

    Fix working on another languages

--- scripts/link.js
+++ scripts/link.js
@@ -3,42 +3,51 @@ const config = { childList: true, subtree: false };
 observer.observe(document.body, config);
 
 function Main() {
-    const check = document.querySelector('.steam-link');
+    try {
+        const steamLink = document.querySelector('.steam-link');
+        if (steamLink) {
+            return;
+        }
 
-    if (check) {
-        return;
+        console.log("Page loaded");
+
+        let parentElement = document.querySelector('.name.clickable.ng-star-inserted');
+        console.log(parentElement);
+
+        const nameSpan = document.querySelector('.mat-mdc-tooltip-trigger');
+        console.log(nameSpan.textContent);
+
+        if (!parentElement) {
+            parentElement = document.querySelector('.name.ng-star-inserted');
+        }
+
+        if (!parentElement || !nameSpan) {
+            return
+        }
+
+        const nameItem = nameSpan.textContent;
+        console.log(nameItem);
+
+        const newLink = document.createElement('a');
+        newLink.href = `https://steamcommunity.com/market/listings/730/${nameItem}`;
+        newLink.target = '_blank';
+        newLink.rel = 'noopener noreferrer';
+        newLink.classList.add('steam-link');
+
+        const steamLogo = document.createElement('img');
+        let imgPath = chrome.runtime.getURL("images/steam_logo.png");
+        steamLogo.src = imgPath;
+        steamLogo.alt = 'Steam';
+        steamLogo.style.width = '32px';
+        steamLogo.style.height = '32px';
+        steamLogo.style.borderRadius = '50%';
+        steamLogo.style.verticalAlign = 'middle';
+        steamLogo.style.marginLeft = '15px';
+
+        newLink.appendChild(steamLogo);
+
+        parentElement.appendChild(newLink);
+    } catch (e) {
+        console.log(e);
     }
-
-    console.log("Page loaded");
-
-    const parentElement = document.querySelector('.name.clickable.ng-star-inserted');
-    const nameSpan = document.querySelector('.mat-mdc-tooltip-trigger');
-
-    if (!parentElement || !nameSpan) {
-        return;
-    }
-
-    const nameItem = nameSpan.textContent;
-    console.log(nameItem);
-
-    const newLink = document.createElement('a');
-    newLink.href = `https://steamcommunity.com/market/listings/730/${nameItem}`;
-    newLink.target = '_blank';
-    newLink.rel = 'noopener noreferrer';
-    newLink.classList.add('steam-link');
-
-
-    const steamLogo = document.createElement('img');
-    let imgPath = chrome.runtime.getURL("images/steam_logo.png")
-    steamLogo.src = imgPath;
-    steamLogo.alt = 'Steam';
-    steamLogo.style.width = '32px'; 
-    steamLogo.style.height = '32px'; 
-    steamLogo.style.borderRadius = '50%'; 
-    steamLogo.style.verticalAlign  = 'middle'; 
-    steamLogo.style.marginLeft = '15px';
-   
-    newLink.appendChild(steamLogo);
-
-    parentElement.appendChild(newLink);
 }
